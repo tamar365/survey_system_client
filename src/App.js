b79b1753b68a5a0c5f29e3a808322a5bd44cc226
tamar365 <tamar.learningd@gmail.com>
2022-06-15 08:44:05 +0300

    open surveyforuser

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Route exact path="/Survey" element={<Survey/>} />
           <Route exact path="/MySurveys" element={<MySurveys/>} />
           <Route exact path="/ExampleOfCreatedSurvey/:idOfSurvey" element={<ExampleOfCreatedSurvey/>} />
-          <Route exact path="/SurveyForUser/:idOfSurvey" element={<SurveyForUser setAccessToUserSurveyPage={setAccessToUserSurveyPage}/>} />
+          <Route exact path="/Surveyforuser/:idOfSurvey" element={<SurveyForUser setAccessToUserSurveyPage={setAccessToUserSurveyPage}/>} />
           <Route exact path="/Answers" element={<Answers/>} />
           <Route exact path="/SendUrlOfSurvey" element={<SendUrlOfSurvey/>} />
         </Routes>
