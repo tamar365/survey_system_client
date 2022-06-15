1b8de20f4167cade9fd13123403f541c8344f965
tamar365 <tamar.learningd@gmail.com>
2022-06-15 14:11:03 +0300

    changes

--- src/Components/SendUrlOfSurvey/SendUrlOfSurvey.jsx
+++ src/Components/SendUrlOfSurvey/SendUrlOfSurvey.jsx
@@ -98,7 +98,7 @@ function SendUrlOfSurvey ({setAccessToUserSurveyPage, setFullURL}) {
                     <a className="anchor"  name="link" href={url} direction="rtl" >לחץ בכדי לעבור לסקר</a>
                     </div> */}
                     <div>
-                      <input className="dinamicUrl" name="my_html"><a href={url}>לחץ בכדי לעבור לסקר</a></input>
+                      <input className="dinamicUrl" name="my_html"><a href={url}>{url}</a></input>
                     </div>
                     <div>
                       <input type="submit" value="שלח"></input>
